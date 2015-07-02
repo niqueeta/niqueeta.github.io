8cae9cea6a0bb16693bc3dcbdfb9a8f0daf308d1
niqueeta <niqueeta@gmail.com>
2015-07-02 17:13:25 +0100

    go

--- javascripts/all.js
+++ javascripts/all.js
@@ -10859,7 +10859,7 @@ $.validate({
 		$.post('application-form.php', $('#apply').serialize())
 			.done(function(data){
 				// Paypal submit
-				alert('Thank you! We Will be in touch soon');
+				alert('Thank you for your application to become a Rainmaker - we shall be in touch by the end of the month. Rainmaker Team.');
 				if ( $('input[value=credit-card]').is(':checked') ) {
 					// console.log('not creditcard');
 					$('form#paypal').submit();
@@ -10889,34 +10889,6 @@ $('input[name=method-of-contact]').click(function (e) {
 	$(this).siblings().css("display", "block");
 });
 
-
-// Message above Submit button changes according to payment method
-function payMessage(message) {
-	$("#last-step").css("display", "block");
-	$("#last-step").html("<legend>" + message + "<label><input id='submit-button' type='submit' name='join' value='Become a Rainmaker'></input></label></legend>");
-}
-
-$('.pay-method input[value=direct-debit]').click(function (e) {
-	payMessage("Thank you! We will send you an email with instructions for how to pay via Direct Debit.");
-});
-
-$('.pay-method input[value=cheque]').click(function (e) {
-	payMessage("Thank you! We will send you an email with instructions for how to pay via cheque.");
-});
-
-$('.pay-method input[value=credit-card]').click(function (e) {
-	payMessage("Thank you! We will now redirect you to a PayPal page where you will be able to pay via Credit Card.");
-});
-
-
-// clicking on div selects input
-$('.membership-type .option').click(function (e) {
-	$(this).siblings().removeClass("selected");
-	$(this).addClass("selected");
-  $(this).find('.option-radio').prop('checked', true);
-});
-
-
 // selecting other text field automatically selects input
 function radioActions(frequency, low) {
 	$('input[type=text]').focus(function (e) {
@@ -10942,44 +10914,12 @@ function donation(frequency, low, mid, high) {
 }
 
 function AnnualValues() {
-	if( $('.membership-type input[value=individual]').is(':checked')) {
-		$('.family').css("display", "block");
-		donation("year", "1,000", "1,500", "2,000");
-
-	}if( $('.membership-type input[value=family]').is(':checked')) {
-		$('.family').css("display", "none");
-		donation("year", "3,000", "4,500", "6,000");
-
-	}if( $('.membership-type input[value=company]').is(':checked')) {
-		$('.family').css("display", "none");
-		donation("year", "5,000", "7,500", "10,000");
-	} else {}
+	donation("year", "1,000", "1,500", "2,000");
 }
 
 function MonthlyValues() {
-	if( $('.membership-type input[value=individual]').is(':checked')) {
-		$('.family').css("display", "block");
-		donation("month", "85", "125", "165");
-
-	}if( $('.membership-type input[value=family]').is(':checked')) {
-		$('.family').css("display", "none");
-		donation("month", "250", "375", "500");
-
-	}if( $('.membership-type input[value=company]').is(':checked')) {
-		$('.family').css("display", "none");
-		donation("month", "415", "625", "835");
-	} else {}
+	donation("month", "85", "125", "165");
 }
-
-$('.membership-type .option').click(function (e) {
-	$(this).siblings().removeClass("selected");
-	$(this).addClass("selected");
-  $(this).find('.option-radio').prop('checked', true);
-  $('.personal-details').css("display", "block");
-  $('#step2').css("display", "block");
-  $('#step3').css("display", "block");
-	AnnualValues();
-});
 
 $('.frequency input[value=annual]').click(function (e) {
 	AnnualValues();
@@ -10991,20 +10931,6 @@ $('.frequency input[value=monthly]').click(function (e) {
 
 
 // Selecting whole box on options checks the child radio button 
-$('.membership-type .option').click(function (e) {
-	$(this).siblings().removeClass("selected");
-	$(this).addClass("selected");
-  $(this).find('.option-radio').prop('checked', true);
-  $('.go-to-step[href="#step2"]').css("display", "block");
-  $('#personal-details').css("display", "block");
-
-	if( $('.membership-type input[value=family]').is(':checked')) {	
-		$('.family').css("display", "block");
-	} else {
-		$('.family').css("display", "none");
-	}
-});
-
 $('.charity-sectors .option').click(function (e) {
 	if( $(this).find('.option-checkbox').is(':checked')) {
 		$(this).removeClass("selected");
@@ -11024,56 +10950,6 @@ $('.donating-time').click(function (e) {
 		$(".areas-of-expertise").css("display", "none");
 	}
 });
-
-$('.partner-donating-time').click(function (e) {
-	if( $(this).is(':checked')) {
-		$(".partner-areas-of-expertise").css("display", "block");
-	} else {
-		$(".partner-areas-of-expertise").css("display", "none");
-	}
-});
-
-
-// Shows partner address if address not the same
-$('input[name=same-address]').click(function (e) {
-	if( $(this).is(':checked')) {
-		$(".partner-address").css("display", "none");
-	} else {
-		$(".partner-address").css("display", "block");
-	}
-});
-
-
-// Shows/hides partner and children details on selection
-$('input[value=partner]').click(function (e) {
-	if( $(this).is(':checked')) {
-		$(".partner-details").css("display", "block");
-	} else {
-		$(".partner-details").css("display", "none");
-	}
-});
-
-$('input[value=child1]').click(function (e) {
-	if( $(this).is(':checked')) {
-		$(".child1-details").css("display", "block");
-	} else {
-		$(".child1-details").css("display", "none");
-	}
-});
-
-
-// Enter partner's interests later
-$('input[value=enter-partner-interests-later]').click(function (e) {
-	if( $(this).is(':checked')) {
-		$(".partner-interests").css("display", "none");
-		$(".partner-interests input").prop('checked', false)
-		$(".partner-areas-of-expertise").css("display", "none");
-		$(".partner-areas-of-expertise").prop('checked', false)
-	} else {
-		$(".partner-interests").css("display", "block");
-	}
-});
-
 (function() {
 
 
